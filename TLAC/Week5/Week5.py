abd8aa78316d03eb01dbccf4a7d5b300a14381ae
Devin Patrick <dtpatrick0706@outlook.com>
2023-09-10 11:58:50 +0900

    completed painting X program

--- TLAC/Week5/Week5.py
+++ TLAC/Week5/Week5.py
@@ -7,14 +7,27 @@ sg.theme("DarkAmber")
 layout = [
     [sg.Text("")]
 ]
-paintings = make_painting(paintings)
+paintings = make_painting(paintings, size)
 
-window = sg.Window("TLAC Week 5", paintings)
+window = sg.Window("TLAC Week 5", paintings, element_justification='c')
 
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "-START-":
+        inc = 1
+        dec = 1
+        for x, row in enumerate(paintings):
+            if x < size:
+                for ele in row:
+                    window["-ELE"+str(inc)+"-"].update(background_color='red')
+                    window["-ELE"+str(size*dec-x)+"-"].update(background_color='red')
+                inc+=size+1
+                dec+=1
+                
+
 
 window.close()
--- TLAC/Week5/painting.py
+++ TLAC/Week5/painting.py
@@ -1,14 +1,38 @@
+# import PySimpleGUI as sg
+# import random
+
+# paintings = []
+# p_size = [3,5,7,9,11,13,15]
+# size = random.choice(p_size)
+
+# def make_painting(paintings,size):
+#     rowNum = 1
+#     for i in range(0,size):
+#         count = 1
+#         row = []
+#         for j in range(0, size):
+#             row.append(sg.Text("", background_color= "yellow", size =(4,2), 
+#                                justification='c', key="-"+str(rowNum)+"ELE"+str(count)+"-"))
+#             count+=1
+#         rowNum+=1
+#         paintings.append(row)
+#     paintings.append([sg.Button("Start", size=(10,2), button_color= 'yellow',key="-START-")])
+#     return paintings
+
 import PySimpleGUI as sg
 import random
 
 paintings = []
 p_size = [3,5,7,9,11,13,15]
+size = random.choice(p_size)
 
-def make_painting(paintings):
-    size = random.choice(p_size)
+def make_painting(paintings, size):
+    count = 1
     for i in range(0,size):
         row = []
         for j in range(0, size):
-            row.append(sg.Text("", background_color= "yellow", size =(4,2), justification='c'))
+            row.append(sg.Text("", background_color= "yellow", size =(4,2), justification='c', key = '-ELE'+str(count)+'-'))
+            count += 1
         paintings.append(row)
+    paintings.append([sg.Button('Start', size = (20, 2), button_color = 'yellow', key = '-START-')])
     return paintings
